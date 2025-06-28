9219f3572c599ac0dd72e5277ea8defdb283c055
chandra-vadamalai98 <chandra.vadamalai98@gmail.com>
2025-06-28 13:44:42 +0530

    Add files via upload
    day1

---
+++ day1/Firefox.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+	
+		float browserVersion = 100.2f;
+		System.out.println("browserVersion is " +browserVersion);
+
+		String browserName = "firefox";
+		System.out.println("browserName is " +browserName);
+		
+		boolean isVisible = true;
+		System.out.println("isVisible is " +isVisible);
+		
+		int releaseYear = 1998;
+		System.out.println("releaseYear is " +releaseYear);
+		
+		char browserLogo = 'f';
+		System.out.println("browserLogo is " +browserLogo);
+		
+	}   
+
+}
---
+++ day1/HelloTestleaf.java
@@ -0,0 +1,12 @@
+package week1.day1;
+
+public class HelloTestleaf {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to testleaf by chandra");
+	
+	}
+	
+	}
+
+
